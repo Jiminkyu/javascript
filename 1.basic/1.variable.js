f667cd1da6d3b0574131834d0b21363804e45d59
coli <coli@coli.com>
2023-02-03 16:15:37 +0900

    2023-02-03

---
+++ 1.basic/1.variable.js
@@ -0,0 +1,21 @@
+`use strict`
+
+/* */
+//
+
+/** variable */
+let user
+let user2
+//let 2user 변수명은 문자로시작해야한다.
+let _user
+let $user
+
+let userName
+let user_name
+//function에 할당하는 변수는 대부분const 로선언한다.
+const x = 1
+//x = 2
+
+let y
+y = 1
+y = 2
